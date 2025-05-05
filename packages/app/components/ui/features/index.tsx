b604ffc5d56328ad04721878a6e079fb401e8808
PetrusRB <pedro.co.dev.ac@gmail.com>
2025-05-05 17:09:16 -0400

    Fix features background.

--- packages/app/components/ui/features/index.tsx
+++ packages/app/components/ui/features/index.tsx
@@ -122,6 +122,7 @@ const styles = StyleSheet.create({
   },
   card: {
     marginBottom: 16,
+    backgroundColor: 'transparent',
     marginRight: 16,
     ...(Platform.OS === 'web' && {
       ':hover': {
